079a2d182445fe6899c97b5f6160163689b251b1
Nathan Babinall <nathanbabinall@gmail.com>
2016-09-10 00:12:49 +1000

    SQLite functionality

--- Project2/Project2/Project2.Droid/Resources/Resource.Designer.cs
+++ Project2/Project2/Project2.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Project2.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::Project2.Droid.Resource.String.ApplicationName;
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::Project2.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_fade_in = global::Project2.Droid.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_fade_out = global::Project2.Droid.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_grow_fade_in_from_bottom = global::Project2.Droid.Resource.Animation.abc_grow_fade_in_from_bottom;
--- Project2/Project2/Project2/ViewModel/AccountsViewModel.cs
+++ Project2/Project2/Project2/ViewModel/AccountsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,27 @@ namespace Project2.ViewModel
 {
     class AccountsViewModel
     {
-        public ObservableCollection<Model.Account> Accounts {get;set;}
+        public ObservableCollection<Model.Account> Accounts { get; set; }
 
         public AccountsViewModel()
         {
-            Accounts = new ObservableCollection<Model.Account>(
-                new Model.Account[] {
-                    new Model.Account { AccountName = "Facebook", Username = "Jack" , Password = "JACKJACK"},
-                    new Model.Account { AccountName = "Deakin", Username = "Nbabinal" , Password = "Poisewoid"},
-                    new Model.Account { AccountName = "GMail", Username = "Dmills@google.com" , Password = "1234" }
-                }
-                );
+            /*Accounts = new ObservableCollection<Model.Account>(
+               new Model.Account[] {
+                   new Model.Account { AccountName = "Facebook", Username = "Jack" , Password = "JACKJACK"},
+                   new Model.Account { AccountName = "Deakin", Username = "Nbabinal" , Password = "Poisewoid"},
+                   new Model.Account { AccountName = "GMail", Username = "Dmills@google.com" , Password = "1234" }
+               }
+               );*/
+            FetchDataAsync();
+        }
+        public void FetchDataAsync()
+        {
+
+            Data.AccountDataAccessService adas = new Data.AccountDataAccessService();
+            List<Model.Account> list = adas.GetAllAccounts();
+            Accounts = new ObservableCollection<Model.Account>(list);
+
+
         }
     }
 }
