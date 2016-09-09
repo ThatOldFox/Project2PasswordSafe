3983f659c57049c562d40adde4e1657f7aa3faee
ThatOldFox <jgallop@deakin.edu.au>
2016-09-09 19:22:14 +1000

    Added login Page and Stared Db Implmentation

---
+++ Project2/Project2/Project2/Data/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.Data
+{
+    class Database
+    {
+        public void Db()
+        {
+            HttpClient Client = new HttpClient();
+            Uri Url = new Uri("http://www.deakin.edu.au/~jgallop/PasswordSafeBackend/getAccounts.php");
+        }
+    }
+}
